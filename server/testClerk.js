f6698462a0cbfdd8127c5aaafa74978d2c86e180
Akshay Singh Thakur <193526154+Thakur-Akshay04@users.noreply.github.com>
2026-06-01 16:03:01 +0530

    fix path import

--- server/testClerk.js
+++ server/testClerk.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+require('dotenv').config({ path: require('node:path').resolve(__dirname, '../.env') });
 const { createClerkClient, verifyToken } = require('@clerk/backend');
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
 console.log('clerkClient keys:', Object.keys(clerkClient));
